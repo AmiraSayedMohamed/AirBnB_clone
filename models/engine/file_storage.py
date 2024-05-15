87af2b8a7a522bd31093091697e080ea2aee9b65
AmiraSayedMohamed <amira.sayedza@gmail.com>
2024-05-15 12:13:06 -0700

    add file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -15,4 +15,23 @@ from models.place import Place
 from models.review import Review
 
 
-
+class FileStorage:
+    """
+    class for storing and retrieving data
+    Class Methods:
+        all: Returns the object
+        new: updates the dictionary id
+        save: Convert Python objects into JSON strings
+        reload: Convert JSON string into Python objects
+    Class Attributes:
+    __file_path (str): Name of file to save Objects to 
+    __objects (dict): Dictionary of instantiated objects
+    class_dict (dict): Dictionry of all the classes
+    """
+    _file_path = "file.json"
+    __objects = {}
+    class_dict = {"BaseModel": BaseModel, "User": User, "Place": Place,
+                  "Amenity": Amenity, "City": City, "Review": Review,
+                  "State": State}
+    
+    
