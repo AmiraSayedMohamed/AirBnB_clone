eaeefa68a3bd5e19c4ecf5c88b59187ec482b946
AmiraSayedMohamed <amira.sayedza@gmail.com>
2024-05-15 11:36:17 -0700

    add file

---
+++ models/engine/file_storage.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" 
+Module for FileStorage class 
+AirBnB cone project
+"""
+import datetime
+import json
+import os
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
+
+
