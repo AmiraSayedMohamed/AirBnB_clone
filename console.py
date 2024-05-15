1d557d1db613de1d5371d397a4a2aeb7423380be
AmiraSayedMohamed <amira.sayedza@gmail.com>
2024-05-15 13:05:33 -0700

    add files

---
+++ console.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+Module for the entry point of the command interpreter
+Defines the HBnB console
+"""
+import cmd
+from models.base_model import BaseModel
+from models import storage
+from shlex import split
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+import re
+import json
+
+
+class HBNBCommand(cmd.Cmd):
+
+    """
+    Class for the command interpreter
+    Defines the HolbrtonBNB command interpreter
+    Attributes:
+    prompt (str): The command prompt
+    """
+
+    prompt = "(hbnb)"
+
+    __classes = {
+        "BaseModel",
+        "User",
+        "State",
+        "City",
+        "Place",
+        "Amenity",
+        "Review"
+    }
+
+     def default(self, line):
+         """ Catchs the command if nothing else matches, 
+         Default behavior for cmd module when input in invalid"""
+        self._precmd(line)
+
---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module created an Amentiy class , a sybclass of BaseModel
+"""
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """
+    class for managing amenity objects, subclass of BaseModel
+    Public class attribute:
+    name: (str)
+    """
+    name = ""
