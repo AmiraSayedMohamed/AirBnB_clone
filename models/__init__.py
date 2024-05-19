5c66203147f87694b4358cba789dbb2692149f45
ahmed711 <ahmed_tayseer12@yahoo.com>
2024-05-19 15:06:15 +0200

    possible fix

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+
 """ Initialized the package magic method for models directory """
+
 from models.engine.file_storage import FileStorage
 storage = FileStorage()
 storage.reload()
